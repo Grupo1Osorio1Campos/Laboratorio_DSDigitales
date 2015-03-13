c7b7cb60b14df47fbccc5d2ee0e43b7345d79cfb
beatle28c <javier28c@hotmail.com>
2015-03-13 15:00:30 -0600

    Carpeta Digitales

---
+++ LabDigitales/Proyecto_Corto_1_LDSD/FlipFlip_JK.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:50:50 02/19/2015 
+// Design Name: 
+// Module Name:    FlipFlip_JK 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FlipFlip_JK(
+);
+
+endmodule
