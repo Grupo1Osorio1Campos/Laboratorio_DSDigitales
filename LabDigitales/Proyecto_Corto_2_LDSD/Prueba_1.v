a34e67a98ae6145f4b6fa23427e251ac345e0d6f
pablofel94 <pablofel0809@gmail.com>
2015-03-16 17:34:27 -0600

    version 2.1.1

--- LabDigitales/Proyecto_Corto_2_LDSD/Prueba_1.v
+++ LabDigitales/Proyecto_Corto_2_LDSD/Prueba_1.v
@@ -18,16 +18,15 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Prueba_1(clk_i,rst_i,ps2_data,ps2_clk,ps2_clk_o,ps2_data_o,rx_en,clk_o,rx_en_o,rx_listo,anodos_o,Q2_o,Q3_o,Q4_o,data_o_o);
+module Prueba_1(clk_i,rst_i,ps2_data,ps2_clk,ps2_clk_o,ps2_data_o,rx_en,clk_o,rx_en_o,rx_listo,anodos_o,Q2_o,Q3_o,Q4_o,data_o_o,En1);
 input wire clk_i,rst_i,ps2_data,ps2_clk,rx_en;
-output wire clk_o,rx_en_o,data_o_o,rx_listo,ps2_clk_o,ps2_data_o,Q3_o,Q4_o;
+output wire clk_o,rx_en_o,data_o_o,rx_listo,ps2_clk_o,ps2_data_o,Q3_o,Q4_o,En1;
 output wire [4:0]Q2_o;
 output wire [3:0] anodos_o;
 
-wire [4:0] temp_o;
-wire En,Q_o,Q1_o;
 wire [7:0] codigo_o;
 wire [8:0] data_o;
+wire [4:0] temp_o;
 //wire clk_o_o_o;
 assign clk_o = clk_o_o;
 assign rx_en_o = rx_en;
@@ -38,7 +37,7 @@ assign ps2_data_o = ps2_data;
 
 ps2 A(clk_i,rst_i,ps2_data,ps2_clk,rx_en,rx_listo,data_o);
 cnvrsr_k_c B(clk_o_o,rst_i,data_o[8:1],codigo_o,anodos_o);
-maquina_de_estados_teclado C(rst_i, clk_i,codigo_o,Q_o,Q1_o,temp_o);
+maquina_de_estados_teclado C(rst_i, clk_i,codigo_o,Q_o,Q1_o,temp_o,En1);
 Registro_5bits E(rst_i, clk_i,temp_o,Q2_o);
 FlipFlop_D F(rst_i, clk_i,Q_o,Q3_o);
 FlipFlop_D G(rst_i, clk_i,Q1_o,Q4_o);
