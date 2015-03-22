b83bbc8f2359a9826073357041100f1dde56d31f
beatle28c <javier28c@hotmail.com>
2015-03-21 21:58:46 -0600

    Scripts para Post Syntesis y Post Place and route
    Simulaciones:
    Post-Synthesis
    Post-Place & route
    Simulacion Synthesis

--- LabDigitales/Proyecto_Corto_2_Version_Final/script_sim.tcl
+++ LabDigitales/Proyecto_Corto_2_Version_Final/script_sim.tcl
@@ -3,8 +3,8 @@
 project compileall;
 
 vsim -gui work.Testbench_Proyecto_C_2;
-	       
-add wave -position insertpoint \
+
+add wave -position insertpoint -color "blue"\
 sim:/Testbench_Proyecto_C_2/clk_i;
 
 add wave -position insertpoint \
@@ -25,15 +25,17 @@ sim:/Testbench_Proyecto_C_2/anodos_o;
 add wave -position insertpoint \
 sim:/Testbench_Proyecto_C_2/codigo_o;
 
-add wave -position insertpoint \
+add wave -position insertpoint -color "green"\
 sim:/Testbench_Proyecto_C_2/Ing_o;
 
-add wave -position insertpoint \
+add wave -position insertpoint -color "green"\
 sim:/Testbench_Proyecto_C_2/Pres_o;
 
-add wave -position insertpoint \
+add wave -position insertpoint -color "red"\
 sim:/Testbench_Proyecto_C_2/Temp_o;
 
 config wave -signalnamewidth 1;
 
-run 1000000ns;
+run 1ms;
+
+wave zoom full;
